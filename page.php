a2fb85bb920afb79bfe5afc36915d9f194f870a7
kalbers <ken.albers@gmail.com>
2021-01-21 13:42:12 -0500

    Add side nav

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
 
       <div class="grid-x grid-padding-x">
-        <div class="large-12 cell">
+        <div class="large-9 cell">
             <h2>A Text Page</h2>
           <p>CSome text. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni consectetur, ex error saepe repudiandae perspiciatis iusto laborum explicabo modi assumenda aut molestias placeat culpa debitis quis optio. Deleniti, amet fugit.</p>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni consectetur, ex error saepe repudiandae perspiciatis iusto laborum explicabo modi assumenda aut molestias placeat culpa debitis quis optio. Deleniti, amet fugit.</p>
@@ -17,6 +17,16 @@
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni consectetur, ex error saepe repudiandae perspiciatis iusto laborum explicabo modi assumenda aut molestias placeat culpa debitis quis optio. Deleniti, amet fugit.</p>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni consectetur, ex error saepe repudiandae perspiciatis iusto laborum explicabo modi assumenda aut molestias placeat culpa debitis quis optio. Deleniti, amet fugit.</p>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni consectetur, ex error saepe repudiandae perspiciatis iusto laborum explicabo modi assumenda aut molestias placeat culpa debitis quis optio. Deleniti, amet fugit.</p>
+        </div>
+        <div class="large-3 cell">
+            <h4>Possible Side Navigation</h4>
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
         </div>
       </div>
 
